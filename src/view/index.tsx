75cd9428f7d9ccc0130119db14f73b8ce5c5c5df
Cầm Văn Thành <thanhcamvan@channelvn.net>
2024-05-17 13:06:36 +0700

    update

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import { firestore } from "../firebase";
-import ReactPlayer from 'react-player'
+// ts
+import {useEffect, useRef, useState} from "react";
+import {useParams} from "react-router-dom"
+import {firestore} from "../firebase";
 
 export const View = () => {
 
-    const { id } = useParams();
+    const {id} = useParams();
+    const video = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
         if (id) {
@@ -14,6 +15,44 @@ export const View = () => {
             })
         }
     }, [id]);
+
+    const showFullScreen = () => {
+        // @ts-ignore
+        if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+            if (video.current?.requestFullscreen) {
+                video.current?.requestFullscreen();
+                // @ts-ignore
+            } else if (video.current?.msRequestFullscreen) {
+                // @ts-ignore
+                video.current?.msRequestFullscreen();
+                // @ts-ignore
+            } else if (video.current?.mozRequestFullScreen) {
+                // @ts-ignore
+                video.current?.mozRequestFullScreen();
+                // @ts-ignore
+            } else if (video.current?.webkitRequestFullscreen) {
+                // @ts-ignore
+                video.current?.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.exitFullscreen) {
+                // @ts-ignore
+                document.exitFullscreen();
+                // @ts-ignore
+            } else if (document.msExitFullscreen) {
+                // @ts-ignore
+                document.msExitFullscreen();
+                // @ts-ignore
+            } else if (document.mozCancelFullScreen) {
+                // @ts-ignore
+                document.mozCancelFullScreen();
+                // @ts-ignore
+            } else if (document.webkitExitFullscreen) {
+                // @ts-ignore
+                document.webkitExitFullscreen();
+            }
+        }
+    }
 
     const [data, setData] = useState<any>(null);
     if (!data) {
@@ -28,13 +67,14 @@ export const View = () => {
             data.type === 'photos'
                 ?
                 (
-                    <img src={data.src} alt="" />
+                    <img src={data.src} alt=""/>
                 )
                 : (
-                    <ReactPlayer url={data.src} controls autoPlay></ReactPlayer>
+                    <video ref={video} src={data.src} controls autoPlay loop></video>
                 )
         }
 
         <a href={data.src} download>{data.name}</a>
+        <button className={'full-screen-btn'} onClick={showFullScreen}>Toàn màn hình</button>
     </div>
 }
